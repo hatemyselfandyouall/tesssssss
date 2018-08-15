284d409b8e1060e75a8b1d0bad786b4746b392e6
高正德 <gaozhengde@zhuojianchina.com>
2018-08-15 19:15:03 +0800

    初始化及thymeleaf引入（未完成

--- src/main/java/cn/ucmed/hlwyy/frontPage/HelloWorld.java
+++ src/main/java/cn/ucmed/hlwyy/frontPage/HelloWorld.java
@@ -1,14 +1,17 @@
 package cn.ucmed.hlwyy.frontPage;
 
+import cn.ucmed.hlwyy.Model.Blog;
 import cn.ucmed.hlwyy.common.ApiResponse;
 import io.swagger.annotations.ApiParam;
 import net.sf.json.JSONObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by YS-GZD-1495 on 2018/7/27.
@@ -17,7 +20,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/helloWorld")
 public class HelloWorld {
     @RequestMapping(value = "/test.htm",  method = RequestMethod.GET)
-    public String test() {
+    public String test(ModelMap map) {
+        List<Blog> blogs=new ArrayList<>();
+        for(int i=0;i<5;i++){
+            Blog blog=new Blog();
+            blog.setName("hello thymeleaf"+i);
+            blog.setContent("hello thymeleaf+i");
+            blogs.add(blog);
+        }
+        map.put("blogs",blogs);
         return "helloWorld/helloWorld";
     }
 
